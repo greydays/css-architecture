8381841d8b164e55c89a014c0386b41bd4a47bee
Ryan Flaherty <rcflaherty@gmail.com>
2015-07-14 12:42:55 -0700

    styling further broken down into modules, fixed copyright symbol, put fonts in scss format for icon fonts, variables put back in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  gulp.src('./app/public/sass/application.sass')
+  gulp.src('./app/public/sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./app/public/css/'));
 });
 
-gulp.task('sass:watch', function(){
-  gulp.watch('./app/public/sass/application.sass', ['sass']);
+gulp.task('default', function(){
+  gulp.watch('./app/public/sass/**/*.sass', ['sass']);
 });
+
 
